e3e2d78ed39546582b81e08da359aeab2c011d45
rennerbernabe <rennerbrian.bernabe@gmail.com>
2024-11-18 10:37:31 +0800

    Move MainActivity

--- app/src/main/java/com/rbb/minireddit/ui/MainActivity.kt
+++ app/src/main/java/com/rbb/minireddit/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.rbb.minireddit
+package com.rbb.minireddit.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -6,11 +6,20 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.rbb.minireddit.ui.screens.home.HomeViewModel
 import com.rbb.minireddit.ui.theme.MiniRedditTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +29,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MiniRedditTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    HomeScreen()
                 }
             }
         }
@@ -31,17 +37,22 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun HomeScreen(
+    viewModel: HomeViewModel = hiltViewModel(), modifier: Modifier = Modifier) {
+
+    val redditPosts by remember { viewModel.redditPosts }.collectAsStateWithLifecycle()
+
+    LazyColumn {
+        items(redditPosts) { post ->
+            Text(text = post.title ?: "", modifier = modifier.padding(16.dp))
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun HomeScreenPreview() {
     MiniRedditTheme {
-        Greeting("Android")
+        HomeScreen()
     }
 }
